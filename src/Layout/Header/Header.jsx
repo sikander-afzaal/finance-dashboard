ba3a13a74842a0d4a8bc94444b67117bdf16b26b
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-04-23 12:06:00 +0500

    first commit

---
+++ src/Layout/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleInfo,
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
+function Header() {
+  return (
+    <div className="header">
+      <div className="left-header">
+        <h1>TITANO</h1>
+      </div>
+      <div className="right-header">
+        <div className="link drop">
+          <div className="top-link">
+            <FontAwesomeIcon icon={faCircleInfo} />
+            <p>TITANO $0.0771 USD</p>
+          </div>
+
+          <div className="dropdown">
+            <div className="drop-link">
+              <p>Buy on Pancake Swap</p>
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </div>
+            <div className="drop-link">
+              <p>Chart</p>
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </div>
+          </div>
+        </div>
+        <div className="link2">
+          <p>Connect Wallet</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
