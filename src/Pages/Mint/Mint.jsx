fa58c44be42239b9106246c673348729d14fca80
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-05-08 17:26:32 +0500

    first commit

--- src/Pages/Mint/Mint.jsx
+++ src/Pages/Mint/Mint.jsx
@@ -21,44 +21,48 @@ function Mint() {
   return (
     <AnimatedPage>
       <div className="mint">
-        <img src={logo} alt="" className="logo-mint" />
-        <h1>Mint an ORCA</h1>
-        <p>1000 “killer whales”</p>
-        <p>1 of 3 collections released over the next 12 months</p>
-        <p>
-          Collect one of each to unlock your pass into our play to earn game
-        </p>
-        <ul>
-          <li>20% reflections to holders </li>
-          <li>20% charity donation</li>
-          <li>15% treasury</li>
-          <li>30% team</li>
-          <li>15% game rewards</li>
-        </ul>
-        <h1>NFT Pricing</h1>
-        <p>Based on a bonding curve</p>
-        <ul>
-          <li>0-50 # Marketing</li>
-          <li>50-100 # 0.1 bnb</li>
-          <li>100-200 # 0.2 bnb</li>
-          <li>200-300 # 0.3 bnb</li>
-          <li>300-400 # 0.4 bnb</li>
-          <li>400-500 # 0.5 bnb</li>
-          <li>500-750 # 0.75 bnb</li>
-          <li> 750-1000 # 1 bnb</li>
-        </ul>
-        <div className="additon-div">
-          <div className="calc">
-            <p onClick={clickHandlerMinus} className="minus">
-              -
-            </p>
-            <p className="number">{array.length}</p>
-            <p onClick={clickHandlerPlus} className="plus">
-              +
-            </p>
+        <div className="left-mint">
+          <h1>Mint your killer whale</h1>
+          <p>2500 “killer whales”</p>
+          <p>1 of 3 collections released over the next 12 months</p>
+          <p>
+            Collect one of each to unlock your pass into our play to earn game
+          </p>
+          <ul>
+            <li>20% reflections to holders </li>
+            <li>20% charity donation</li>
+            <li>15% treasury</li>
+            <li>30% team</li>
+            <li>15% game rewards</li>
+          </ul>
+          <h1>NFT Pricing</h1>
+          <p>Based on a bonding curve</p>
+          <ul>
+            <li>0-50 # Marketing</li>
+            <li>50-100 # 0.1 bnb</li>
+            <li>100-200 # 0.2 bnb</li>
+            <li>200-300 # 0.3 bnb</li>
+            <li>300-400 # 0.4 bnb</li>
+            <li>400-500 # 0.5 bnb</li>
+            <li>500-750 # 0.75 bnb</li>
+            <li> 750-1000 # 1 bnb</li>
+          </ul>
+          <div className="additon-div">
+            <div className="calc">
+              <p onClick={clickHandlerMinus} className="minus">
+                -
+              </p>
+              <p className="number">{array.length}</p>
+              <p onClick={clickHandlerPlus} className="plus">
+                +
+              </p>
+            </div>
           </div>
+          <button className="mint-btn">Mint</button>
         </div>
-        <button className="mint-btn">Mint</button>
+        <div className="right-mint">
+          <img src={logo} alt="" />
+        </div>
       </div>
     </AnimatedPage>
   );
--- src/Pages/View/View.jsx
+++ src/Pages/View/View.jsx
@@ -5,13 +5,8 @@ function Mint() {
   return (
     <AnimatedPage>
       <div className="view">
-        <h1>View your Orca</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quos
-          dolores voluptas, dolorem voluptate molestiae! Molestiae, commodi
-          quibusdam. Ipsam nemo sunt cumque eos eius quo labore? Vel itaque illo
-          ab.
-        </p>
+        <h1>Connect with your wallet to view your purchased “killer whales”</h1>
+        <p>So much more than Just an Nft</p>
       </div>
     </AnimatedPage>
   );
